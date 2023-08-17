f6540badf11445cab715c1416106cd012b96d9f9
Ahmed Amin <AhmedAminHassanIsmail@gmail.com>
2023-08-17 16:45:06 +0300

    attaches the app assets and create launch screen

---
+++ SwiftUIAvocadoRecipes/SwiftUIAvocadoRecipes/SwiftUIAvocadoRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAvocadoRecipesApp.swift
+//  SwiftUIAvocadoRecipes
+//
+//  Created by Ahmed Amin on 17/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAvocadoRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
